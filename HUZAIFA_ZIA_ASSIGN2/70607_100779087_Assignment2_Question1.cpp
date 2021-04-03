f8d0e315a6ed3b545cb4ea199a19e307a9c449e5
Huzaifa Zia <57328992+huzaifazia17@users.noreply.github.com>
2021-04-03 14:11:59 -0400

    Add files via upload

--- HUZAIFA_ZIA_ASSIGN2/70607_100779087_Assignment2_Question1.cpp
+++ HUZAIFA_ZIA_ASSIGN2/70607_100779087_Assignment2_Question1.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-    string criteria1 = "Idea";
-    string criteria2 = "Novelty";
-    string criteria3 = "Contribution";
-    string criteria4 = "Paper Organization";
-    string criteria5 = "Paper Evaluation";
-    string criteria[5] = {criteria1, criteria2, criteria3, criteria4, criteria5};
-
+    string criteria[5] = {"Idea", "Novelty", "Contribution", "Paper Organization", "Paper Evaluation"};
     int reviewers[4][5];
     double AVG_1{0}, AVG_2{0}, AVG_3{0}, AVG_4{0};
     double TOTAL_AVG;
@@ -24,42 +17,30 @@ int main()
         cout << "Criteria " << i + 1 << " is " << criteria[i] << endl;
     }
 
-    for (int i = 1; i <= 4; i++)
+    for (int i = 0; i < 4; i++)
     {
-        for (int j = 1; j <= 5; j++)
+        for (int j = 0; j < 5; j++)
         {
-            cout << "Enter a grade for Criteria " << j << " of Reviewer " << i << " : ";
-            cin >> reviewers[i - 1][j - 1];
-            while (reviewers[i - 1][j - 1] < 0 || reviewers[i - 1][j - 1] > 10)
+            cout << "Enter a grade for Criteria " << j + 1 << " of Reviewer " << i + 1 << "  : ";
+            cin >> reviewers[i][j];
+            while (reviewers[i][j] < 0 || reviewers[i][j] > 10)
             {
-                cout << "Enter a Grade Greater than 0 and less than 10 for Criteria " << j << " of Reviewer " << i << " : ";
-                cin >> reviewers[i - 1][j - 1];
+                cout << "Enter a grade Greater than 0 and less than 10 for Criteria " << j + 1 << " of Reviewer " << i + 1 << " : ";
+                cin >> reviewers[i][j];
             }
         }
     }
 
-    for (int j = 0; j <= 4; j++)
+    for (int i = 0; i <= 4; i++)
     {
-        AVG_1 += reviewers[0][j];
+        AVG_1 += reviewers[0][i];
+        AVG_2 += reviewers[1][i];
+        AVG_3 += reviewers[2][i];
+        AVG_4 += reviewers[3][i];
     }
     AVG_1 = AVG_1 / 5;
-
-    for (int j = 0; j <= 4; j++)
-    {
-        AVG_2 += reviewers[1][j];
-    }
     AVG_2 = AVG_2 / 5;
-
-    for (int j = 0; j <= 4; j++)
-    {
-        AVG_3 += reviewers[2][j];
-    }
     AVG_3 = AVG_3 / 5;
-
-    for (int j = 0; j <= 4; j++)
-    {
-        AVG_4 += reviewers[3][j];
-    }
     AVG_4 = AVG_4 / 5;
 
     TOTAL_AVG = (AVG_1 + AVG_2 + AVG_3 + AVG_4) / 4;
